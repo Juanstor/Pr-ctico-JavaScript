6afde2554ddde2f2673838d0c8e5fbd12ba4dde0
Juanstor <alvarezjuansbs@gmail.com>
2023-01-24 12:34:42 -0500

    add mobile menu, config responsive desing, delete display in styles/mobile-menu

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// Menu desktop
+
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+// Mobile Desktop
+
+const burgerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+burgerMenu.addEventListener('click', toggleMobileMenu)
+function toggleMobileMenu() {
+    console.log('Clickeaste en la burger')
+    mobileMenu.classList.toggle('inactive')
 }
