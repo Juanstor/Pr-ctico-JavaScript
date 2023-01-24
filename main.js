0cead837ced6dfe448cf0cc9bbc617b5ebf2c288
Juanstor <alvarezjuansbs@gmail.com>
2023-01-24 16:28:24 -0500

    conditional in process, make menus close after click

--- main.js
+++ main.js
@@ -24,8 +24,18 @@ function toggleMobileMenu() {
 const cartMenuIcon = document.querySelector('.navbar-shopping-card')
 const aside = document.querySelector('.product-detail')
 
-cartMenuIcon.addEventListener('click', toggleCartMenu)
-function toggleCartMenu() {
+
+
+// En esta parte hay que darle click a un menu y que los otros se cierren para uqe no se superpongan, hay otra solucion que es simplemente agregar el inactive cada vez que se le da clic sin necesidad de saber con if si esta abierto o cerrado los otros menus. 
+
+cartMenuIcon.addEventListener('click', toggleCartAside)
+function toggleCartAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains ('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
     aside.classList.toggle('inactive')
 }
 
