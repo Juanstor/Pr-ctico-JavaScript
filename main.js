ead2b2dd3dc70e9884cf724d18c229debf22a17e
Juanstor <alvarezjuansbs@gmail.com>
2023-01-25 21:51:10 -0500

    re organize JS, change class=aside for cartAside, put another if to solve severals open menuS. SOLVE ISSUE

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 // Desktop Menu
-
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+// Mobile Desktop - Burguer menu
+const burgerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+// Cart Menu
+const cartMenuIcon = document.querySelector('.navbar-shopping-card')
+const cartAside2 = document.querySelector('.cartAside')
+
+
+
+// functions (same order)
 menuEmail.addEventListener('click', toggleDesktopMenu)
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-// Mobile Desktop
 
-const burgerMenu = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
 
 burgerMenu.addEventListener('click', toggleMobileMenu)
+
 function toggleMobileMenu() {
+
+    const isCartAsideClosed = cartAside2.classList.contains('inactive');
+    if (!isCartAsideClosed) {
+        cartAside2.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive')
 }
 
 
-// Cart Menu
-
-const cartMenuIcon = document.querySelector('.navbar-shopping-card')
-const aside = document.querySelector('.product-detail')
-
-
-
-// En esta parte hay que darle click a un menu y que los otros se cierren para uqe no se superpongan, hay otra solucion que es simplemente agregar el inactive cada vez que se le da clic sin necesidad de saber con if si esta abierto o cerrado los otros menus. 
 
 cartMenuIcon.addEventListener('click', toggleCartAside)
+
 function toggleCartAside() {
 
     const isMobileMenuClosed = mobileMenu.classList.contains ('inactive');
@@ -36,6 +44,6 @@ function toggleCartAside() {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive')
+    cartAside2.classList.toggle('inactive')
 }
 
